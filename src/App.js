4983f837a73a08498c38df0cb2e202d47eef54f0
SOFIA DOMINGUEZ GOMEZ <sofiadominguezgomez03@gmail.com>
2022-11-14 00:43:21 -0300

    try

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="rubinwebpage/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
